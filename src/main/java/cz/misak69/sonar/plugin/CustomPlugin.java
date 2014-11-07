0cf74d8950ce777954e8a57fe094f192203e4d3f
misak69 <jenicek.michal@gmail.com>
2014-11-07 07:58:26 +0100

    Javadoc update.

--- src/main/java/cz/misak69/sonar/plugin/CustomPlugin.java
+++ src/main/java/cz/misak69/sonar/plugin/CustomPlugin.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (C) 2009-2014 SonarSource SA
- * All rights reserved
- * mailto:contact AT sonarsource DOT com
- */
 package cz.misak69.sonar.plugin;
 
 import com.google.common.collect.ImmutableList;
